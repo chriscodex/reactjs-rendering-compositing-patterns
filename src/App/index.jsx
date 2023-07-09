5422d875e736fdb8ae8c130644a96629ff975e3b
Christian <c.espinoza.code@gmail.com>
2023-07-09 18:40:22 -0500

    component composition pattern used in app index

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,64 @@
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../context/TodoContext';
+import { TodoHeader } from '../components/TodoHeader';
+import { TodoCounter } from '../components/TodoCounter';
+import { TodoSearch } from '../components/TodoSearch';
+import { TodoList } from '../components/TodoList';
+import { TodoItem } from '../components/TodoItem';
+import { CreateTodoButtom } from '../components/CreateTodoButtom';
+import { TodosLoading } from '../components/TodosLoading';
+import { TodosError } from '../components/TodosError';
+import { EmptyTodos } from '../components/EmptyTodos';
+import { Modal } from '../components/Modal';
+import { TodoForm } from '../components/TodoForm';
+import { useTodos } from './useTodos';
 
 function App() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    onComplete,
+    onDelete,
+    openModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    setOpenModal,
+    addTodo,
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  )
+    <>
+      <TodoHeader>
+        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      </TodoHeader>
+      <TodoList>
+        {loading && <TodosLoading />}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <EmptyTodos />}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => onComplete(todo.text)}
+            onDelete={() => onDelete(todo.text)}
+          />
+        ))}
+      </TodoList>
+      <CreateTodoButtom setOpenModal={setOpenModal} />
+      {openModal && (
+        <Modal>
+          <TodoForm
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            addTodo={addTodo}
+          ></TodoForm>
+        </Modal>
+      )}
+    </>
+  );
 }
 
 export default App;
