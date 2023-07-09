c3ee8558176f1a3acbe9444bafd7e2bfbcbaeeba
Christian <c.espinoza.code@gmail.com>
2023-07-09 14:28:58 -0500

    todoList now it uses render prop pattern

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,13 +1,26 @@
-import './TodoList.css'
+import './TodoList.css';
 
-function TodoList(props){
-    return(
-        <section>
-            <ul>
-                {props.children}
-            </ul>
-        </section>
-    );
+function TodoList({
+  error,
+  loading,
+  searchedTodos,
+  onLoading,
+  onError,
+  onEmpty,
+  renderItem,
+  children,
+}) {
+  return (
+    <section className="TodoList-container">
+    {error && onError()}
+      {loading && onLoading()}
+      {(!loading && searchedTodos.length === 0) && onEmpty()}
+      {searchedTodos.map((todo) => (
+        renderItem(todo)
+      ))} 
+      <ul>{children}</ul>
+    </section>
+  );
 }
 
-export { TodoList};
+export { TodoList };
