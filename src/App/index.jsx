7adf4a97d6c42651b4de9aca7f2558b0188b88f2
Christian <c.espinoza.code@gmail.com>
2023-07-10 18:12:39 -0500

    loading property deleted from todoCounter & todoSearch components and moved to Todoheader component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -30,16 +30,14 @@ function App() {
 
   return (
     <>
-      <TodoHeader>
+      <TodoHeader loading={loading}>
         <TodoCounter
           totalTodos={totalTodos}
           completedTodos={completedTodos}
-          loading={loading}
         />
         <TodoSearch
           searchValue={searchValue}
           setSearchValue={setSearchValue}
-          loading={loading}
         />
       </TodoHeader>
 
