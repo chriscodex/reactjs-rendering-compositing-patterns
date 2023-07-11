dca2731506cab19a1f13226e1da5dd2efb2db101
Christian <c.espinoza.code@gmail.com>
2023-07-11 17:09:35 -0500

    changeAlert component html modal structure created

--- src/components/ChangeAlert/index.jsx
+++ src/components/ChangeAlert/index.jsx
@@ -1,14 +1,24 @@
+import './changeAlert.css'
 import { withStorageListener } from './withStorageListener';
 
 function ChangeAlert({ show, toggleShow }) {
   if (show) {
     return (
-      <div>
-        <p>ChangeAlert</p>
+      <div className="ChangeAlert-bg">
+        <div className="ChangeAlert-container">
+          <p>Parece que cambiaste tus TODOs en otra pestaña o ventana del navegador.</p>
+          <p>¿Quieres sincronizar tus TODOs?</p>
+          <button
+            className="TodoForm-button TodoForm-button--add"
+            onClick={toggleShow}
+          >
+            Sincronizar
+          </button>
+        </div>
       </div>
     );
   } else {
-    return null
+    return null;
   }
 }
 
