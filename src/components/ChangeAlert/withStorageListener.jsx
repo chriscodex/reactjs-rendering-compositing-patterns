abeacefd8988b0264a876af2322e5041773f59e2
Christian <c.espinoza.code@gmail.com>
2023-07-11 13:16:17 -0500

    component withStorageListener created

---
+++ src/components/ChangeAlert/withStorageListener.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function withStorageListener(WrappedComponent) {
+  return function WrappedComponentWithStorageListener(props) {
+    const [storageChange, setStorageChange] = useState(false);
+    
+    return (
+      <WrappedComponent show={storageChange} toggleShow={setStorageChange}/>
+    )
+  }
+}
+
+export { withStorageListener }
